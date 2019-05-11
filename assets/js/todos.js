89485357d95dd00f011755c14d9472001b9935b3
stelthdroid8 <brandon.kennedy1994@gmail.com>
2019-05-11 13:12:34 -0400

    added proper fadeout and deletion to removal of todo

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -2,3 +2,12 @@
 $("li").on("click", function(){
     $(this).toggleClass("completed");
 });
+
+//allows click on "X" to delete the toDo
+$("span").on("click", function(event){
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    });
+    
+    event.stopPropagation();
+});
